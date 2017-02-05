347c03bf1c327f52a1a906ed103c80844e959da5
Zbyszek <zbigniew.wolfigiel@gmail.com>
2017-02-05 01:04:17 +0100

    Widgets and fields added on SearchActivity

--- app/src/main/java/com/example/zbyszek/ute/SearchActivity.java
+++ app/src/main/java/com/example/zbyszek/ute/SearchActivity.java
@@ -1,15 +1,22 @@
 package com.example.zbyszek.ute;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 
 // Activity collecting user preferences about places and distance
 public class SearchActivity extends AppCompatActivity {
+
+    float distance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +25,25 @@ public class SearchActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+        String[] places = {"Kino", "Pub", "Przystanek", "Bankomat"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, places);
+        Spinner spinner = (Spinner)findViewById(R.id.spinnerPlaces);
+        spinner.setAdapter(adapter);
+
+        EditText editText = (EditText)findViewById(R.id.editTextDistance);
+
+
+
+        Button button = (Button)findViewById(R.id.searchBtn2);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view){
+                // action
             }
         });
+
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
